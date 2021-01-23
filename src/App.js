25398ef8118fd7db416ec45ea4672298db175463
Valerie Ingmann <valerieingmann707@gmail.com>
2021-01-23 15:25:39 -0500

    useState

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { FaStar } from "react-icons/fa";
+
+const createArray = length => [...Array(length)];
+const Star = ({ selected = false, onSelect }) => {
+  return <FaStar color={selected ? "red" : "gray"} onClick={onSelect} />;
+};
+const StarRating = ({ totalStars = 5 }) => {
+  const [selectedStars, setSelectedStars] = useState(0);
+  return createArray(totalStars).map((n, i) => (
+    <Star key={i} selected={selectedStars > i} onSelect={() => setSelectedStars(i + 1)} />
+  ));
+};
 
 function App() {
+  const [status, setStatus] = useState(false);
+  const [checked, setChecked] = useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <h1>The package is: {status ? "Delivered" : "Not Delivered"}</h1>
+        <button onClick={() => setStatus(!status)}>Deliver</button>
+      </div>
+      <div>
+        <input type="checkbox" value={checked} onChange={() => setChecked(!checked)} />
+      </div>
+      <StarRating totalStars={10} />
+    </>
   );
 }
 
