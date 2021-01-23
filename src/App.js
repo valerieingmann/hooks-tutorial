2276a69a5fd7b36cbda2da74fe05a420863e2472
Valerie Ingmann <valerieingmann707@gmail.com>
2021-01-23 15:55:18 -0500

    useRef

--- src/App.js
+++ src/App.js
@@ -1,70 +1,24 @@
-import React, { useEffect, useState } from "react";
-import { FaStar } from "react-icons/fa";
+import React, { useEffect, useState, useReducer, useRef } from "react";
 
-const createArray = length => [...Array(length)];
-const Star = ({ selected = false, onSelect }) => {
-  return <FaStar color={selected ? "red" : "gray"} onClick={onSelect} />;
-};
-const StarRating = ({ totalStars = 5 }) => {
-  const [selectedStars, setSelectedStars] = useState(0);
-  return createArray(totalStars).map((n, i) => (
-    <Star key={i} selected={selectedStars > i} onSelect={() => setSelectedStars(i + 1)} />
-  ));
-};
+const App = () => {
+  const sound = useRef();
+  const color = useRef();
 
-function App() {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const res = await fetch("https:/api.github.com/users");
-      const data = await res.json();
-      setData(data);
-    };
-    fetchData();
-  }, []);
-
+  const submit = e => {
+    e.preventDefault();
+    const soundVal = sound.current.value;
+    const colorVal = color.current.value;
+    alert(`${soundVal} sounds like ${colorVal}`);
+    sound.current.value = "";
+    color.current.value = "";
+  };
   return (
-    <>
-      <ul>
-        {data.map(user => (
-          <li key={user.id}>{user.login}</li>
-        ))}
-      </ul>
-      <button onClick={() => setData([])}>Remove</button>
-    </>
+    <form onSubmit={submit}>
+      <input ref={sound} type="text" placeholder="Sound..." />
+      <input ref={color} type="color" />
+      <button>ADD</button>
+    </form>
   );
-  // const [status, setStatus] = useState(false);
-  // const [checked, setChecked] = useState(false);
-  // const [name, setName] = useState("Valerie");
-  // const [admin, setAdmin] = useState(false);
-
-  // useEffect(() => {
-  //   document.title = `Celebrate ${name}`;
-  // }, [name]);
-
-  // useEffect(() => {
-  //   console.log(`user is ${admin ? "admin" : "not admin"}`);
-  // }, [admin]);
-
-  // return (
-  //   <>
-  //     <div>
-  //       <h1>The package is: {status ? "Delivered" : "Not Delivered"}</h1>
-  //       <button onClick={() => setStatus(!status)}>Deliver</button>
-  //     </div>
-  //     <div>
-  //       <input type="checkbox" value={checked} onChange={() => setChecked(!checked)} />
-  //     </div>
-  //     <StarRating totalStars={10} />
-  //     <section>
-  //       <p>Congratulations {name}</p>
-  //       <button onClick={() => setName("Rosie")}>Change Winner</button>
-  //       <p>{admin ? "logged in" : "not logged in"}</p>
-  //       <button onClick={() => setAdmin(true)}>Log In</button>
-  //     </section>
-  //   </>
-  // );
-}
+};
 
 export default App;
